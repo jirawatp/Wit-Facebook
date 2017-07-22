4e9aa44d3c93dddb83f8211153a4a3d522bf13e5
Jirawat Pattanayutanachot <jirawat.p@cout-dev.com>
2017-07-22 23:36:33 +0700

    try to handle loc in getStatus action

--- bot.js
+++ bot.js
@@ -71,9 +71,15 @@ const actions = {
   },
 
   // getStatusr bot executes
-  ['getStatus'](sessionId, context, cb) {
+  ['getStatus'](sessionId, context, entities, message, cb) {
     // Here should go the api call, e.g.:
     // context.forecast = apiCall(context.loc)
+
+    const loc = firstEntityValue(entities, 'location') || entityValue(entities, "loc");
+    if (loc) {
+      context.loc = loc; // store it in context
+    }
+    
     console.log("meeeeee");
     console.log(JSON.stringify(context));
     if (context.entities || context.loc) {
